33b05334a82f6ae52e90b7efd4ac1e675396df55
Chenxia Han <chenxia.han@whu.edu.cn>
2019-11-17 17:39:04 +0800

    add scripts

---
+++ scripts/infer.sh
@@ -0,0 +1,2 @@
+./darknet detector test cfg/car.data cfg/yolov3-tiny-car.cfg vdata/Lamai-Walking-Street/checkpoint/yolov3-tiny-car_final.weights data/dog.jpg
+#./darknet detector test cfg/coco.data cfg/yolov3.cfg ../darknet/yolov3.weights data/dog.jpg
---
+++ scripts/map.sh
@@ -0,0 +1,1 @@
+./darknet detector map cfg/car.data cfg/yolov3-tiny-car.cfg vdata/Lamai-Walking-Street/checkpoint/yolov3-tiny-car_final.weights
